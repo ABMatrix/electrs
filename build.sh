2c00a615a872b95e8334f43f30610d82bebca876
kayryu <lukayryu@gmail.com>
2023-04-12 19:52:16 -0700

    feat: add build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# fail fast on any non-zero exits
+set -e
+
+# the docker image name and dockerhub repo
+NAME="bnk-btc-electrs"
+REPO="boolnetwork"
+
+VERSION=`head -n 3 ./Cargo.toml | egrep -o "([0-9\.]+)"`
+
+echo $VERSION
+
+echo "*** Building $NAME"
+docker build -t $NAME -f Dockerfile .
+
+docker login -u $REPO -p $DOCKER_PASS
+
+echo "*** Tagging $REPO/$NAME"
+if [[ $VERSION != *"beta"* ]]; then
+  docker tag $NAME $REPO/$NAME:$VERSION
+fi
+docker tag $NAME $REPO/$NAME
+
+echo "*** Publishing $NAME"
+docker push $REPO/$NAME
